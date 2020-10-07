027902452e662c474d7d02f2ed2ca93e0b3e1a06
tptarun <tarunprajapati151@gmail.com>
2020-10-07 20:47:10 +0530

    update

--- script.js
+++ script.js
@@ -85,3 +85,29 @@ class TextScramble {
   };
   
   next();
+
+ // Cache selectors
+var lastId,
+topMenu = $("#mainNav"),
+topMenuHeight = topMenu.outerHeight()+1,
+// All list items
+menuItems = topMenu.find("a"),
+// Anchors corresponding to menu items
+scrollItems = menuItems.map(function(){
+  var item = $($(this).attr("href"));
+   if (item.length) { return item; }
+});
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function(e){
+ var href = $(this).attr("href"),
+	 offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+ $('html, body').stop().animate({ 
+	 scrollTop: offsetTop
+ }, 850);
+ e.preventDefault();
+});
+
+//scroll
+
